9fc04bb2c7ab4fe425483091f1e6974a04ba4e56
nhaatjlong <longdn555@gmail.com>
2021-11-25 14:08:30 +0700

    [002] add card product

---
+++ components/CardProduct/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Product } from "../../pages";
+import styles from "./CartProduct.module.scss";
+import locationImg from "../../assets/location.png";
+export interface CardProductProps {
+  product: Product;
+}
+
+export default function CardProduct({ product }: CardProductProps) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card__left}>
+        <img src="https://commerce.bidu.com.vn//uploads/images/products/1636558504857_image_605917.jpeg" />
+        <div className={styles.rank}>{product.id}</div>
+      </div>
+      <div className={styles.card__right}>
+        <div className={styles.name}>{product.name}</div>
+        <div className={styles.price}>{product.price}</div>
+        <div className={styles.location}>{product.location}</div>
+        <div className={styles.sold}>Đã bán {product.sold}</div>
+      </div>
+    </div>
+  );
+}
