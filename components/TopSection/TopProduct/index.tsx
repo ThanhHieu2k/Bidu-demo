5e8bdb2cf6db7578f13a6b0d0bd3dcaa400c93e1
nhaatjlong <longdn555@gmail.com>
2021-11-29 00:45:48 +0700

    [002] create top product component

--- components/TopSection/TopProduct/index.tsx
+++ components/TopSection/TopProduct/index.tsx
@@ -1,27 +1,26 @@
 import * as React from "react";
-import { Button, Col } from "reactstrap";
-import { Product } from "../../../pages";
-import CardProduct from "../../CardProduct";
+import { Product } from "../../../data";
+import { CardProduct } from "../../Card";
 
 import styles from "./TopProduct.module.scss";
 
 export interface TopProductProps {
-  listProduct: Product[];
+  topProductList: Product[];
 }
 
 export function TopProduct(props: TopProductProps) {
-  const { listProduct } = props;
-  console.log(listProduct);
+  const { topProductList } = props;
+  console.log(topProductList);
   return (
-    <Col lg="3" className={styles.topProduct}>
+    <div className={styles.topProduct}>
       <div className={styles.topProduct__head}>
         <span>TOP SẢN PHẨM</span>
       </div>
       <div className={styles.topProduct__body}>
-        {listProduct.map((product) => {
+        {topProductList.map((product) => {
           return <CardProduct product={product} key={product.id} />;
         })}
       </div>
-    </Col>
+    </div>
   );
 }
