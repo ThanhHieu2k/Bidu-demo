0a5e55ce15187f1adc6f6d1a046cb5171f2742e7
nhaatjlong <longdn555@gmail.com>
2021-12-01 14:55:07 +0700

    [002] create models

---
+++ models/top_product.ts
@@ -0,0 +1,12 @@
+export default class TopProductModel {
+  private readonly id: number;
+  private readonly name: string;
+  private readonly price: string;
+  private readonly location: string;
+  private readonly img: StaticImageData;
+  private readonly sold: number;
+
+  constructor(props: any) {
+      
+  }
+}
