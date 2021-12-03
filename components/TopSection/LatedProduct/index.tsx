84e927e8a6d1324a6bef3aba39c8239dd270c1d4
nhaatjlong <longdn555@gmail.com>
2021-12-04 02:20:28 +0700

    [002] add slider for newest product

--- components/TopSection/LatedProduct/index.tsx
+++ components/TopSection/LatedProduct/index.tsx
@@ -1,14 +1,47 @@
-import * as React from "react";``
+import Slider from "react-slick";
+import * as React from "react";
+``;
 import { ITopProduct } from "../../../models/topProducts";
 import { CardLatedProduct } from "../../Card";
 import styles from "./LatedProduct.module.scss";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export interface LatedProductProps {
   latedProductList: ITopProduct[];
 }
 
 export default function LatedProduct(props: LatedProductProps) {
   const { latedProductList } = props;
+  const PrevArrow = ({ onClick }: any) => {
+    return (
+      <div className={styles.arrowPrev} onClick={onClick}>
+        <img
+          src="https://www.bidu.com.vn/images/icon/category_back.svg"
+          alt="Arrow Prev"
+        />
+      </div>
+    );
+  };
+  const NextArrow = ({ onClick }: any) => {
+    return (
+      <div className={styles.arrowNext} onClick={onClick}>
+        <img
+          src="https://www.bidu.com.vn/images/icon/category_next.svg"
+          alt="Arrow Next"
+        />
+      </div>
+    );
+  };
+  const setting = {
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: 0,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
   return (
     <div className={styles.latedProduct}>
       <div className={styles.latedProduct__head}>
@@ -17,9 +50,13 @@ export default function LatedProduct(props: LatedProductProps) {
         </span>
       </div>
       <div className={styles.latedProduct__body}>
-        {latedProductList.map((product) => {
-          return <CardLatedProduct product={product} key={product.id} />;
-        })}
+        <div className={styles.slider}>
+          <Slider {...setting} className={styles.slider__content}>
+            {latedProductList.map((product) => {
+              return <CardLatedProduct product={product} key={product.id} />;
+            })}
+          </Slider>
+        </div>
       </div>
     </div>
   );
