f9ffb22ad09f172dbf4d03a04034be514f1e4963
nhaatjlong <longdn555@gmail.com>
2021-12-04 00:06:31 +0700

    [002] config env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  "presets": ["next/babel"]
-}
+  presets: ["next/babel"],
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
+};
