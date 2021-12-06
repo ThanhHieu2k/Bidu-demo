c3e2ce2c773123ed97b3712a7b26a71a86e6507b
Vinh127 <huynhxuanvinh120700@gmail.com>
2021-12-06 18:16:00 +0700

    feate/login-signup Finished

--- components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import Skeleton from "react-loading-skeleton";
-
-export const LoadingProduct = () => {
-  return <Skeleton height={127} width={127} duration={5} />;
-};
-
-export const LoadingAvatar = () => {
-  return <Skeleton circle={true} height={82} width={82} />;
-};
